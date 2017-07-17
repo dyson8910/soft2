4d119717468019ec4cca79c92ee8d18669199e0b
dyson8910 <mech.b.dyson@gmail.com>
2017-07-17 09:31:00 +0900

    from text

---
+++ ch02/fact.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FALSE 0
+#define TRUE 1
+
+int Debug = FALSE;
+
+int fact(int x){
+  if(x>0){
+    if(Debug){
+      printf("x = %d\n",x);
+    }
+    return (x * fact(x-1));
+  }else{
+    if(Debug){
+    printf("x = %d, return 1\n",x);
+    }
+    return 1;
+  }
+}
+
+int main(int argc,char* argv[]){
+  int x, ret;
+  while((argc > 1) && (argv[1][0] == '-')){
+    switch (argv[1][1]) {
+      case  'd':Debug = TRUE;break;
+    }
+    argc--; argv++;
+  }
+  x = atoi(argv[1]);
+  printf("x:%d\n",x);
+  ret = fact(x);
+  printf("%d\n",ret);
+  return 0;
+}
